2a3fc411669ec33c59aacf219d4cc6dca159d670
skanders00 <131609323+skanders00@users.noreply.github.com>
2026-02-24 11:57:05 +0100

    Delete Spring Boot Backoffice directory

--- Spring Boot Backoffice/Certificate/src/main/java/tn/esprit/spring/exambl/Repository/QuestionOptionRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.spring.exambl.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.exambl.Entity.QuestionOption;
-
-public interface QuestionOptionRepository extends JpaRepository<QuestionOption, Long> {
-}
--- Spring Boot Backoffice/Certificate/src/main/java/tn/esprit/spring/exambl/Repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.spring.exambl.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tn.esprit.spring.exambl.Entity.Question;
-
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
