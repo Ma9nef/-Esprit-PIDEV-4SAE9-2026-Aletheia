056050649ed45e3951543d40afd2a3be977227af
skanders00 <131609323+skanders00@users.noreply.github.com>
2026-02-24 11:57:05 +0100

    Delete Spring Boot Backoffice directory

--- Spring Boot Backoffice/Certificate/src/main/java/tn/esprit/spring/exambl/Repository/SubmissionRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.spring.exambl.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.spring.exambl.Entity.QuestionOption;
-import tn.esprit.spring.exambl.Entity.Submission;
-
-public interface SubmissionRepository extends JpaRepository<Submission, Long> {
-}
