e243c1a5170991757ddd33b8396da4acfba8f004
Manef Akrimi <fenam.akrimi@gmail.com>
2026-03-12 23:33:32 +0100

    resolve conflicts

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -16,11 +16,12 @@ import { ExploreMegaMenuComponent } from './explore-mega-menu/explore-mega-menu.
     CourseCardComponent,
     ExploreMegaMenuComponent
   ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        CommonModule,
+        RouterModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
   exports: [
     NavbarComponent,
     FooterComponent,
