7755a96a9ec2d30a86b5d141419b86ded53b170b
Ma9nef <fenam.akrimi@gmail.com>
2026-05-06 16:07:31 +0100

    now it'll work

--- backend/ApiGateway/src/main/java/com/esprit/microservice/apigateway/CorsConfig.java
+++ backend/ApiGateway/src/main/java/com/esprit/microservice/apigateway/CorsConfig.java
@@ -7,18 +7,41 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsWebFilter corsWebFilter() {
+
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOriginPatterns(java.util.Arrays.asList("http://localhost:4200"));
-        corsConfig.setAllowedMethods(java.util.Arrays.asList("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
-        corsConfig.setAllowedHeaders(java.util.Arrays.asList("*"));
+
+        corsConfig.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:4200",
+                "http://192.168.49.2:30080"
+        ));
+
+        corsConfig.setAllowedMethods(Arrays.asList(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS",
+                "PATCH"
+        ));
+
+        corsConfig.setAllowedHeaders(Arrays.asList("*"));
+
         corsConfig.setAllowCredentials(true);
+
         corsConfig.setMaxAge(3600L);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource(
+                        new PathPatternParser()
+                );
+
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
